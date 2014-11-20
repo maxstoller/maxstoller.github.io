8c6242e56d923bc0377fabe8b034ed2a40e830f6
maxstoller <maxstoller@gmail.com>
2014-11-19 21:56:23 -0500

    Reduce playbackRate and remove log msg.

--- core.js
+++ core.js
@@ -1,9 +1,8 @@
 $(function() {
-  $('video.one').get(0).playbackRate = 1.6;
-  $('video.two').get(0).playbackRate = 1.6;
+  $('video.one').get(0).playbackRate = 1.5;
+  $('video.two').get(0).playbackRate = 1.5;
 
   $('video.one').on('ended', function(event) {
-    console.log('ended');
     $('video.two').get(0).play();
   });
 
